281fd331d1a2e0923aed39d60cdce09d23bc9d95
Pheonix <Yashasewi@gmail.com>
2022-07-04 20:19:56 +0530

    just formatted

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,21 +2,21 @@ import React from 'react'
 
 export default function Header() {
   return (
-        <div>
-            <header>
-            <img className='logo' src='/images/logo.svg' alt ="logo"></img>  
-            <nav className='nav-bar'>
-                <ul className='nav-list'>
-                <li className='items'>Features</li>
-                <li className='items'>Pricing</li>
-                <li className='items'>Resources</li>
-                </ul>
-            </nav>
-                <ul className='nav-bar-button'>
-                <button className='login'>Login</button>
-                <button className='sign_up'>Sign up</button>
-                </ul>
-            </header>
-        </div>
-  )
+    <div>
+      <header>
+        <img className="logo" src="/images/logo.svg" alt="logo"></img>
+        <nav className="nav-bar">
+          <ul className="nav-list">
+            <li className="items">Features</li>
+            <li className="items">Pricing</li>
+            <li className="items">Resources</li>
+          </ul>
+        </nav>
+        <ul className="nav-bar-button">
+          <button className="login">Login</button>
+          <button className="sign_up">Sign up</button>
+        </ul>
+      </header>
+    </div>
+  );
 }
