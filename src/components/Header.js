e51e2e06dc87246de8255463e1df1ec0ef2ff811
Pheonix <Yashasewi@gmail.com>
2022-07-04 14:50:52 +0530

    written css for header

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,17 +2,21 @@ import React from 'react'
 
 export default function Header() {
   return (
-    <div >
-      <h1 className='heading'>Shortly</h1>
-      <ul className='resources'>
-        <li>Features</li>
-        <li>Pricing</li>
-        <li>Resources</li>
-      </ul>
-      <ul className='login'>
-        <li>Login</li>
-        <li>Sign up</li>
-      </ul>
-    </div>
+        <div>
+            <header>
+            <img className='logo' src='/images/logo.svg' alt ="logo"></img>  
+            <nav className='nav-bar'>
+                <ul className='nav-list'>
+                <li className='items'>Features</li>
+                <li className='items'>Pricing</li>
+                <li className='items'>Resources</li>
+                </ul>
+            </nav>
+                <ul className='nav-bar-button'>
+                <button className='login'>Login</button>
+                <button className='sign_up'>Sign up</button>
+                </ul>
+            </header>
+        </div>
   )
 }
