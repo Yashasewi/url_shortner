e5369ff9edf832b4b175a171ffff174aeb9f3a86
Pheonix <Yashasewi@gmail.com>
2022-07-05 18:21:37 +0530

    just changed some intro section

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,25 +6,28 @@ export default function Footer() {
         <footer className='footer'>
             <h4>Shortly</h4>
             <p>A simple URL shortener</p>
-            <span>Features</span>
-            <ul>
-                <li>Link Shortening</li>
-                <li> Branded Links</li>
-                <li>Analytics</li>
-            </ul>
-            <span>Resources</span>
-            <ul>
-                <li>Blog</li>
-                <li>Developers</li>
-                <li>Support</li>
-            </ul>
-            <span>Company</span>
-            <ul>
-                <li>About</li>
-                <li>Our Team</li>
-                <li>Careears</li>
-                <li>Contacts</li>
-            </ul>
+            <article>Features
+                <ul>
+                    <li>Link Shortening</li>
+                    <li> Branded Links</li>
+                    <li>Analytics</li>
+                </ul>
+            </article>
+            <article>Resources
+                <ul>
+                    <li>Blog</li>
+                    <li>Developers</li>
+                    <li>Support</li>
+                </ul>
+            </article>
+            <article>Company
+                <ul>
+                    <li>About</li>
+                    <li>Our Team</li>
+                    <li>Careers</li>
+                    <li>Contacts</li>
+                </ul>
+            </article>
         <ul className='footer_icons'>
             <li><img src="https://img.icons8.com/ios/50/000000/facebook-new.png" alt="facebook logo" /></li>
             <li><img src="https://img.icons8.com/ios/50/000000/instagram-new--v1.png" alt="insta logo"/></li>
--- src/components/intro.js
+++ src/components/intro.js
@@ -3,15 +3,16 @@ import React from 'react'
 export default function Intro() {
   return (
     <div className = 'intro'>
-        <p className='text'>
-            <img  className ='illustration_working' src='/images/illustration-working.svg'  alt = "working logo" />
-            <h1 className='quote'>More than just shorter links</h1>
-            <p className = 'inside_text'>Build your brand's recognition and get detailed insights 
+            <p className = 'inside_text'>
+            <h1 className='quote'>More Than Just<br/>Shorter Links</h1>
+            Build your brand's recognition and get detailed<br/> insights 
             on how your links are performing.
             </p>
-            <img className='searchbar' src = '/images/searchbar.svg' alt = "search bar" />
-        </p>
-        {/* <img src = '../images/illustration-working.svg' alt='wokrking logo' /> */}
+            <img  className ='img' src='/images/illustration-working.svg'  alt = "working logo" />
+            <img className='search_bar_img' src = '/images/searchbar.svg' alt = "search bar" />
+        {/* <img src = '../images/illustration-working.svg' alt='working logo' /> */}
     </div>
   )
 }
+
+
