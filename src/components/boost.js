81c18a923d2fde22dadd7acad277a6ca068f15eb
Pheonix <Yashasewi@gmail.com>
2022-07-09 01:53:47 +0530

    fixed boost

--- src/components/boost.js
+++ src/components/boost.js
@@ -2,9 +2,10 @@ import React from 'react'
 
 export default function Boost() {
   return (
-    <div>
-        <h1 className='boost_text'>Boost your links today</h1>
-        <button className='button_started'>Get Started</button>
+    <div className="boost">
+    
+      <h1 className="boost_text">Boost your links today</h1>
+      <button className="sign_up bo">Get Started</button>
     </div>
-  )
+  );
 }
