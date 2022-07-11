a62507a2f0e287d5443c63f9b2cab1e43beabfad
Pheonix <Yashasewi@gmail.com>
2022-07-11 11:35:27 +0530

    cahnged name

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import Header from './components/Header';
-import Intro from './components/Introo';
+import Introo from './components/Introo';
 import Url from './components/Url';
 import Statistics from './components/Statistics';
 import Boost from './components/Boost';
